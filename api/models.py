786af3f880de264c912d3ea64357fd684401e447
devKOfori <groupfivethree@gmail.com>
2024-11-30 23:13:13 +0200

    Create models and townserializer

--- api/models.py
+++ api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Region(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    name = models.CharField(max_length=255, db_index=True)
+
+    class Meta:
+        db_table = "region"
+
+    def __str__(self) -> str:
+        return self.name
+
+class District(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    name = models.CharField(max_length=255, db_index=True)
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        db_table = "district"
+
+    def __str__(self) -> str:
+        return self.name
+
+class Town(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    name = models.CharField(max_length=255, db_index=True)
+    district = models.ForeignKey(District, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        db_table = "town"
+
+    def __str__(self) -> str:
+        return self.name
